3d2ee7ff3ef3e9adb896bcfccdf38475b0f0065f
peter.ringuelet@gmail.com <peter.ringuelet@gmail.com>
2023-05-29 13:34:54 -0300

    Falta probar en WinMips

---
+++ Modulo2/Practica4/8.s
@@ -0,0 +1,20 @@
+.data 
+NUM1: .word 2
+NUM2: .word 3
+RES: .word 0
+
+.code
+ld r1, NUM1(r0)     ; numero 1
+ld r2, NUM2(r0)     ; numero 2
+dadd r3, r0, r0     ; resultado
+
+beqz r1, final              ; si NUM1 = 0 no hago nada (RES = 0 por defecto)
+beqz r2, final              ; si NUM2 = 0 no hago nada (RES = 0 por defecto)
+loop: dadd r3, r3, r1       ; RES = RES + NUM1 (trabajo con r3, no con RES)
+daddi r2, r2, -1            ; decremento NUM2
+bnez r2, loop               ; si no termine de mult, sigo
+
+sd r3, RES(r0)       ; pongo en RES el valor del contador (r3)
+final: nop
+halt
+; falta probar en Windows
