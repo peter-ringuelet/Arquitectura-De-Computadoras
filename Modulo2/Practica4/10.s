ebad68f0407f114e5cb4326a4c011c33fda29be6
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-05-29 22:56:05 -0300

    cantidad de veces que un char esta en una cadena

--- Modulo2/Practica4/10.s
+++ Modulo2/Practica4/10.s
@@ -1,3 +1,4 @@
+.data
 cadena: .asciiz "adbdcdedfdgdhdid"  ; cadena a analizar
 car: .asciiz "d"                    ; caracter buscado
 cant: .word 0                       ; cantidad de veces que se repite el caracter car en cadena.
@@ -10,14 +11,12 @@ dadd r1, r0, r0         ; pos donde estoy
 
 loop: beqz r3, fin      ; si llegue al final termino
 lbu r5, cadena(r1)      ; pongo en r5 letra(pos)
-bne r5, r3, sigo        ; si letra(pos) != car => sigo
+bne r5, r2, sigo        ; si letra(pos) != car => sigo
 daddi r4, r4, 1         ; si es aumento contador
-sigo: daddi, r1, r1, 1  ; aumeneto pos
+sigo: daddi r1, r1, 1  ; aumeneto pos
 daddi r3, r3, -1        ; disminuyo cant letras cadena
 j loop                  ; continuo con el loop
 
 fin: sd r4, cant(r0)    ; pongo lo que hay en el contador de r4 en cant
 nop                     ; nunca entendi que hace
 halt
-
-;revisra en WinMips
